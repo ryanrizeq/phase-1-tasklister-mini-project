286a77683ecf898f8f485e560443e45cc01f9146
Ryan Rizeq <ryanrizeq@gmail.com>
2023-03-29 18:19:35 -1000

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const taskList = document.getElementById('tasks');
+    newTask = document.createElement('li');
+    newTask.textContent = document.getElementById("create-task-form").elements['new-task-description'].value;
+    taskList.append(newTask);
+    event.target.reset();
+  })
 });
